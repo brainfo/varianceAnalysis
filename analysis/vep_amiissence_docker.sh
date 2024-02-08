df564827d6dffa067415aebc22a2ac16a3f72464
brainfo <11832080+brainfo@users.noreply.github.com>
2024-02-08 04:34:19 +0100

    Update vep_amiissence_docker.sh

--- analysis/vep_amiissence_docker.sh
+++ analysis/vep_amiissence_docker.sh
@@ -1,1 +1,4 @@
-
+docker run -t -i -u root -v /mnt/data/hong/courses/varianceAnalysis/data:/data ensemblorg/ensembl-vep
+INSTALL.pl -a cf -s homo_sapiens -y GRCh38
+tabix -s 1 -b 2 -e 2 -f -S 1 AlphaMissense_hg38.tsv.gz
+vep -i ADA_variant_rsid.txt --plugin AlphaMissense,file=AlphaMissense_hg38.tsv.gz -o ADA_variant_rsid_vep_aMissense.txt --cache --force_overwrite
